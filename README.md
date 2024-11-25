091a345e2c2709ce8dad53d026eddc215aabfc8c
Laurenz Pichler <l.pichler@htlkrems.at>
2024-11-25 10:18:36 +0100

    Test

--- README.md
+++
@@ -1,1 +0,0 @@
-"# KremsGuesser" 
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Alle Session-Variablen löschen
-session_destroy(); // Session beenden
-header("Location: index.php"); // Zur Startseite weiterleiten
-exit();
-?>
