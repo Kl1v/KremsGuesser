d433847623b3912c7aae0349a5dec162007d239c
janTiefenbacher <160015456+janTiefenbacher@users.noreply.github.com>
2024-12-16 08:31:41 +0100

    abc

--- play.php
+++ play.php
@@ -1,10 +1,5 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_name'])) {
-    // Benutzer ist nicht angemeldet, leitet auf die Login-Seite weiter
-    header('Location: login.php');
-    exit;
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,6 +11,39 @@ if (!isset($_SESSION['user_name'])) {
     <link rel="stylesheet" href="stylemain.css">
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <style>
+
+        .raise:hover,
+        .raise:focus {
+            box-shadow: 0 0.5em 0.5em -0.4em white; /* Weißer Schatten */
+            transform: translateY(-0.25em); /* Button hebt sich */
+        }
+
+        button {
+            background: none;
+            border: 2px solid;
+            font: inherit;
+            line-height: 1;
+            margin: 0.5em;
+            padding: 3em 2em;
+            color: var(--color); 
+            transition: 0.25s; 
+        }
+
+        button:hover,
+        button:focus {
+            border-color: var(--hover); /* Optional: Rahmenfarbe ändern */
+            box-shadow: 0 0.5em 0.5em -0.4em white; /* Weißer Schatten */
+        }
+
+        /* Beispiel für Farbvariablen */
+        :root {
+            --color: #ffa260; /* Standardfarbe */
+            --hover: #ffc78e; /* Hover-Farbe */
+        }
+        
+    </style>
 </head>
 
 <body style="padding-top: 70px;">
@@ -23,20 +51,24 @@ if (!isset($_SESSION['user_name'])) {
     <?php require 'navbar.php'; ?>
 
     <!-- Content Section -->
-    <div class="container">
+    <div class="container" data-aos="fade-down" data-aos-duration="1000">
         <div class="play-container">
-            <h1>Modi Wählen</h1>
-            <h4>Allein üben oder Freunde herausfordern?</h4>
+            <h1>Modus wählen</h1>
+            <h4>Allein spielen oder Freunde herausfordern?</h4>
             <div class="d-flex flex-column align-items-center gap-3">
-                <a href="singleplayer.php"><button type="submit" class="btn-custom">Singleplayer</button></a>
+                <a href="singleplayer.php"><button type="button" class="btn-custom raise">Singleplayer</button></a>
                 <form action="Multiplayer.php" method="get">
-                    <button type="submit" class="btn-custom">Multiplayer</button>
+                    <button type="submit" class="btn-custom raise">Multiplayer</button>
                 </form>
             </div>
         </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
 
 </html>
